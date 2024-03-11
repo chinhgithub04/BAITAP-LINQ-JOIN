e5acb78d15f4ca86ebb67786e4e9957ebab19a69
Nguyễn Đức Chinh <Nguyn c Chinh@JOEL-MILLER>
2024-03-11 22:27:30 +0700

    Add project files.

---
+++ BaiTap/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapLinQ
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int DepartmentID { get; set; }
+        public static List<Employee> getEmployee()
+        {
+            return new List<Employee>
+            {
+                new Employee { ID = 1, Name = "A", Age = 22, Salary = 2000, Birthday = DateTime.Parse("21/02/2002"), DepartmentID = 1},
+                new Employee { ID = 2, Name = "B", Age = 25, Salary = 2500, Birthday = DateTime.Parse("22/08/1999"), DepartmentID = 2},
+                new Employee { ID = 3, Name = "C", Age = 23, Salary = 2000, Birthday = DateTime.Parse("11/09/2001"), DepartmentID = 3 },
+                new Employee { ID = 4, Name = "D", Age = 40, Salary = 4000, Birthday = DateTime.Parse("10/12/1984"), DepartmentID = 1},
+                new Employee { ID = 5, Name = "E", Age = 33, Salary = 3200, Birthday = DateTime.Parse("05/09/1991"), DepartmentID = 2}
+            };
+        }
+
+        public int CalculateAge(DateTime BirthDay)
+        {
+            DateTime Today = DateTime.Now;
+            return Today.Year - Birthday.Year;
+        }
+    }
+}
---
+++ BaiTap/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace BaiTapLinQ
+{
+    public class Run
+    {
+        public static void Main(string[] args)
+        {
+            //Cau2a
+            //Max Salary
+            var maxSalary = Employee.getEmployee().Max(x => x.Salary);
+            Console.WriteLine("Max salary is: " + maxSalary);
+
+            //Min Salary
+            var minSalary = Employee.getEmployee().Min(x => x.Salary);
+            Console.WriteLine("Min salary is: " + minSalary);
+
+            //Average Salary
+            var averageSalary = Employee.getEmployee().Average(x => x.Salary);
+            Console.WriteLine("Average salary is: " + averageSalary);
+            //End 2a
+
+            //Cau 2b
+            //GroupJoin
+            Console.WriteLine("\n\nGroupJoin");
+            var groupJoin = Department.getDepartments()
+                                      .GroupJoin(Employee.getEmployee(),
+                                      d => d.ID, e => e.DepartmentID,
+                                      (department, employee) => new
+                                      {
+                                          Department = department,
+                                          Employee = employee
+                                      });
+            foreach (var department in groupJoin)
+            {
+                Console.WriteLine("Department: " + department.Department.Name);
+                foreach (var employee in department.Employee)
+                {
+                    Console.WriteLine("EmployeeID: " + employee.ID + ", EmployeeName: " + employee.Name);
+                }
+                Console.WriteLine();
+            }
+
+            //LeftJoin
+            Console.WriteLine("\n\nLeft join");
+            var leftJoin = from e in Employee.getEmployee()
+                                join d in Department.getDepartments()
+                                on e.DepartmentID equals d.ID into eGroup
+                                from d in eGroup.DefaultIfEmpty()
+                                select new
+                                {
+                                    EmployeeName = e.Name,
+                                    DepartmentName = d == null ? "No Department" : d.Name
+                                };
+            foreach(var employee in leftJoin)
+            {
+                Console.WriteLine(employee.EmployeeName + "-" + employee.DepartmentName);
+            }
+
+            //RightJoin
+            Console.WriteLine("\n\nRight join");
+            var rightJoin = from d in Department.getDepartments()
+                                join e in Employee.getEmployee()
+                                on d.ID equals e.DepartmentID into eGroup
+                                from e in eGroup.DefaultIfEmpty()
+                                select new
+                                {
+                                    DepartmentName = d.Name,
+                                    EmployeeName = e == null ? "No employees" : e.Name
+                                };
+            foreach (var department in rightJoin)
+            {
+                Console.WriteLine(department.DepartmentName + "-" + department.EmployeeName);
+            }
+            //End 2b
+
+            //Cau 2c
+            //Max age
+            Console.WriteLine("\n\n");
+            var maxAge = Employee.getEmployee().Max(e => e.CalculateAge(e.Birthday));
+            Console.WriteLine("Max age: " + maxAge);
+
+            var minAge = Employee.getEmployee().Min(e => e.CalculateAge(e.Birthday));
+            Console.WriteLine("Min age: " + minAge);
+        }
+    }
+}
